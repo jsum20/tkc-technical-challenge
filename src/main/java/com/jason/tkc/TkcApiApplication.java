b7bdab84e130fbb5a08999e96ae63f092964a62a
Jason Sum <jasonsum321@gmail.com>
2025-02-18 16:10:45 +0000

    Initial Commit - Setting up Springboot and database connection

---
+++ src/main/java/com/jason/tkc/TkcApiApplication.java
@@ -0,0 +1,13 @@
+package com.jason.tkc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jason/tkc/TkcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.tkc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
